0ec0947b143a031b4c02ba04e297ce2beda23876
Thet Minn Htun <thetminnhtun92@gmail.com>
2021-11-06 15:40:14 +0630

    Install CoreUI

---
+++ resources/views/layouts/navigation.blade.php
@@ -0,0 +1,18 @@
+<ul class="sidebar-nav" data-coreui="navigation" data-simplebar>
+    <li class="nav-item">
+        <a class="nav-link" href="{{ route('home') }}">
+            <svg class="nav-icon">
+                <use xlink:href="{{ asset('icons/coreui.svg#cil-speedometer') }}"></use>
+            </svg>
+            {{ __('Dashboard') }}
+        </a>
+    </li>
+        <li class="nav-item">
+        <a class="nav-link" href="{{ route('about') }}">
+            <svg class="nav-icon">
+                <use xlink:href="{{ asset('icons/coreui.svg#cil-speedometer') }}"></use>
+            </svg>
+            {{ __('About us') }}
+        </a>
+    </li>
+</ul>
