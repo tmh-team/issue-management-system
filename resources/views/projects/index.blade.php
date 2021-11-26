1a2038252ab97a307dbfdfa4a1b28001cf17e1d7
KhineZinThaw7 <88822096+KhineZinThaw7@users.noreply.github.com>
2021-11-27 01:14:43 +0630

    add project new feature

--- resources/views/projects/index.blade.php
+++ resources/views/projects/index.blade.php
@@ -1,25 +1,23 @@
 @extends('layouts.app')
 
 @section('content')
+<x-flash.success-alert/>
 <div class="row mb-4">
     <div class="col-6 offset-6 text-end">
         <a href="{{ route('projects.create') }}" class="btn btn-primary btn-sm">@lang('Create')</a>
     </div>
 </div>
-
 <div class="card mb-4">
     <div class="card-header">
         {{ __('Project List') }}
     </div>
     <div class="card-body">
-
-
         <table class="table table-striped">
             <thead>
                 <tr>
                     <th scope="col">#</th>
                     <th scope="col">@lang('Name')</th>
-                    <th scope="col" style="width: 300px;">@lang('Actions')</th>
+                    <th scope="col" style="width: 350px;">@lang('Actions')</th>
                 </tr>
             </thead>
             <tbody>
@@ -43,7 +41,11 @@
                                 <a href="{{ route('projects.edit', $project->id) }}" class="btn btn-success btn-sm">
                                     @lang('Edit')
                                 </a>
-                                <button type="submit" class="btn btn-danger btn-sm">
+                                 <a href="{{ route('projects.show', $project->id) }}" class="btn btn-info btn-sm">
+                                    @lang('Detail')
+                                </a>
+                                <button type="submit" class="btn btn-danger btn-sm"
+                                        onclick="return confirm('Are you sure want to delete?')">
                                     @lang('Delete')
                                 </button>
                             </form>
