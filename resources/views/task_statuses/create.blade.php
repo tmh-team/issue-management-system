ce1487e9460453226c2e51a84649b8ccd57f5089
PhyoHtetAungSCM <scm.phyohtetaung@gmail.com>
2021-12-01 00:40:12 +0630

    add status color (frontend)

--- resources/views/task_statuses/create.blade.php
+++ resources/views/task_statuses/create.blade.php
@@ -12,9 +12,24 @@
                 <label class="form-label">@lang('Status')</label>
                 <input name="status" class="form-control" type="text">
                 @error('status')
-                    <p class="text-danger text-xs">{{ $message }}</p>  
+                <p class="text-danger text-xs">{{ $message }}</p>
                 @enderror
             </div>
+
+            <div class="row mb-3">
+                <div class="col-md-1">
+                    <label class="form-label">@lang('Color')</label>
+                    <input class="form-control" type="color" style="height: 38px" oninput="getColorValue()">
+                </div>
+                <div class="col-md-1">
+                    <label class="form-label">@lang('Hex:')</label>
+                    <input name="color" class="form-control" type="text" maxlength="7" oninput="changeColorValue(this)">
+                    @error('color')
+                    <p class="text-danger text-xs">{{ $message }}</p>
+                    @enderror
+                </div>
+            </div>
+
             <div class="d-flex justify-content-between">
                 <button type="submit" class="btn btn-primary">@lang('Create')</button>
                 <a href="{{ route('statuses.index', $projectId) }}" class="btn btn-outline-secondary">@lang('Back')</a>
@@ -22,4 +37,8 @@
         </form>
     </div>
 </div>
+@endsection
+
+@section('script')
+<script src="/js/task_status/color.js"></script>
 @endsection
--- resources/views/task_statuses/edit.blade.php
+++ resources/views/task_statuses/edit.blade.php
@@ -17,6 +17,21 @@
                     <p class="text-danger text-xs">{{ $message }}</p>  
                 @enderror
             </div>
+
+            <div class="row mb-3">
+                <div class="col-md-1">
+                    <label class="form-label">@lang('Color')</label>
+                    <input class="form-control" type="color" style="height: 38px" oninput="getColorValue()">
+                </div>
+                <div class="col-md-1">
+                    <label class="form-label">@lang('Hex:')</label>
+                    <input name="color" class="form-control" type="text" maxlength="7" oninput="changeColorValue(this)">
+                    @error('color')
+                    <p class="text-danger text-xs">{{ $message }}</p>
+                    @enderror
+                </div>
+            </div>
+
             <div class="d-flex justify-content-between">
                 <button type="submit" class="btn btn-primary">@lang('Update')</button>
                 <a href="{{ route('statuses.index', $projectId) }}" class="btn btn-outline-secondary">@lang('Back')</a>
@@ -24,4 +39,8 @@
         </form>
     </div>
 </div>
+@endsection
+
+@section('script')
+<script src="/js/task_status/color.js"></script>
 @endsection
