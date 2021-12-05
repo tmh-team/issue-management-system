8ca453dc3bf5357e9f685af866e9179b03c6933d
Thet Minn Htun <thetminnhtun92@gmail.com>
2021-12-05 16:25:16 +0630

    View 404 if get MethodNotAllowedHttpException

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -3,6 +3,7 @@
 namespace App\Exceptions;
 
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
+use Symfony\Component\HttpKernel\Exception\MethodNotAllowedHttpException;
 use Throwable;
 
 class Handler extends ExceptionHandler
@@ -36,6 +37,10 @@ class Handler extends ExceptionHandler
     {
         $this->reportable(function (Throwable $e) {
             //
+        });
+
+        $this->renderable(function (MethodNotAllowedHttpException $e) {
+            abort(404);
         });
     }
 }
